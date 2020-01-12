b04c31acdf7c447b3d42d48f94f6c5de03ad872d
Chris Cummins <chrisc.101@gmail.com>
2020-01-12 17:33:26 +0000

    Add an install script.
    [Exported from e7d4ec7d91efc64189b24262b7d6fe98aff9e1a4]

--- tools/format/install.sh
+++ tools/format/install.sh
@@ -8,12 +8,9 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
   source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
   source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=
-# --- begin app init ---
-
-set -eu
+--- begin app init ---
 
 PREFIX=$HOME/.local
 
 mkdir -pv "$PREFIX/bin"
-rm -vf "$PREFIX/bin/format"
 cp -v "$(DataPath phd/tools/format/format.par)" "$PREFIX/bin/format"
