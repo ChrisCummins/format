133e6ce1313c77086e46a445426052e830decbd9
Chris Cummins <chrisc.101@gmail.com>
2020-01-12 16:40:26 +0000

    Work in progress on pre-commit mode.
    [Exported from d9f1cc75625639a15640478d256d6774233f2351]

--- tools/format/git_util.py
+++ tools/format/git_util.py
@@ -12,15 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Utilities for interfacing with git."""
-import os
 import pathlib
 import subprocess
 import sys
-from typing import Iterable
 from typing import List
-from typing import Tuple
 
-import build_info
 from labm8.py import app
 
 FLAGS = app.FLAGS
@@ -54,84 +50,3 @@ def GetModifiedFilesOrDie(staged: bool) -> List[pathlib.Path]:
   lines = output.split("\n")
   staged_file_relpaths = lines[:-1]  # Last line is blank.
   return staged_file_relpaths
-
-
-def GetStagedPathsOrDie(
-  path_generator,
-) -> Tuple[List[pathlib.Path], List[pathlib.Path]]:
-  os.chdir(GetGitRootOrDie())
-
-  staged_paths, partially_staged_paths = [], []
-
-  staged_relpaths = GetModifiedFilesOrDie(staged=True)
-  unstaged_relpaths = GetModifiedFilesOrDie(staged=False)
-  partially_staged_relpaths = list(
-    set(staged_relpaths).intersection(set(unstaged_relpaths))
-  )
-
-  for relpath in staged_relpaths:
-    paths = list(path_generator.GeneratePaths([relpath]))
-    # A staged path can resolve to multiple real paths if it is a submodule
-    # that is being altered. Ignore those.
-    if len(paths) > 1:
-      continue
-    if paths:
-      staged_paths.append(paths[0])
-
-  for relpath in partially_staged_relpaths:
-    paths = list(path_generator.GeneratePaths([relpath]))
-    # A staged path can resolve to multiple real paths if it is a submodule
-    # that is being altered. Ignore those.
-    if len(paths) > 1:
-      continue
-    if paths:
-      partially_staged_paths.append(paths[0])
-
-  return staged_paths, partially_staged_paths
-
-
-def GitAddOrDie(paths: Iterable[pathlib.Path]):
-  try:
-    subprocess.check_call(["git", "add"] + [str(x) for x in paths])
-  except subprocess.CalledProcessError:
-    print(f"ERROR: git add faled: {paths}", file=sys.stderr)
-    sys.exit(1)
-
-
-def InstallPreCommitHookOrDie(cache_path: pathlib.Path):
-  git_root = GetGitRootOrDie()
-
-  hooks_dir = git_root / ".git" / "hooks"
-
-  if not hooks_dir.is_dir():
-    print(f"ERROR: git hooks directory not found: {hooks_dir}", file=sys.stderr)
-    sys.exit(1)
-
-  # Install pre-commit hook which runs this program in --pre_commit mode.
-  pre_commit = hooks_dir / "pre-commit"
-  if pre_commit.is_file():
-    os.unlink(pre_commit)
-
-  with open(pre_commit, "w") as f:
-    f.write(f"#!/usr/bin/env bash\nset -eu\n{sys.argv[0]} --pre_commit\n")
-  os.chmod(pre_commit, 0o744)
-  print(pre_commit)
-
-  # Install prepare-commit-msg hook which adds a "signed off" hook.
-  prepare_commit_msg = hooks_dir / "prepare-commit-msg"
-  if prepare_commit_msg.is_file():
-    os.unlink(prepare_commit_msg)
-
-  version = build_info.GetBuildInfo().version
-  with open(prepare_commit_msg, "w") as f:
-    f.write(
-      f"""\
-#!/usr/bin/env bash
-cp $1 {cache_path / 'git_commit_message.txt'}
-echo -e "\\n\\nSigned-off-by: format {version} <github.com/ChrisCummins/format>" > $1
-cat {cache_path / 'git_commit_message.txt'} >> $1
-rm -f {cache_path / 'git_commit_message.txt'}
-"""
-    )
-  os.chmod(prepare_commit_msg, 0o744)
-  print(prepare_commit_msg)
